b843f68de11c33d5c4955257862ec635e3945bd4
Connor Oliver <connor@muezza.ca>
2025-05-07 13:36:08 -0300

    Remove abandoned router projects

--- NhdRouter/src/main/java/ca/nhd/comm/exceptions/CommError.java
+++
@@ -1,7 +0,0 @@
-package ca.nhd.comm.exceptions;
-
-public class CommError extends RuntimeException {
-    public CommError(String errorMessage, Throwable err) {
-        super(errorMessage, err);
-    }
-}
--- NhdRouter/src/main/java/ca/nhd/configuration/DeviceConfig.java
+++
@@ -1,7 +0,0 @@
-package ca.nhd.configuration;
-
-public class DeviceConfig {
-    public static void loadDevices(){
-        new ObjectMapper().readValue(new ClassPathResource("./defaults/myjson.json").getFile(), Pojo.class);
-    }
-}
